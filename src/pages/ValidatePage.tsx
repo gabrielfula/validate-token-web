ac7e0472e69925c7426f2818c71dc59114a04055
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-08-29 23:37:05 -0300

    create page and button to open app

--- src/pages/ValidatePage.tsx
+++ src/pages/ValidatePage.tsx
@@ -1,7 +1,43 @@
+import { ShieldCheckIcon } from "lucide-react";
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import "../styles/validate-page.css"
+
 export default function ValidatePage() {
+  const { hash } = useParams();
+
+  const handleOpenApp = () => {
+    const appUrl = 'yourapp://';
+    const fallbackUrl = 'https://play.google.com/store/apps/details?id=com.example.yourapp';
+  
+    window.location.href = appUrl;
+  
+    setTimeout(() => {
+      window.location.href = fallbackUrl;
+    }, 1000);
+  };
+  
+
+  useEffect(() => {
+    //fazer requisiçao para a api
+  }, [hash])
+
   return (
     <>
-      <div>ValidatePage</div>
+      <div className="container">
+        <div className="title-content">
+          <div>
+            <ShieldCheckIcon />
+            <h1>Conta verificada</h1>
+          </div>
+          <div className="content">
+            <p>Parabéns! Seu e-mail foi confirmado com sucesso. Agora você está pronto para explorar todas as funcionalidades do nosso aplicativo. Volte para o aplicativo e inicie sua jornada!</p>
+            <button onClick={handleOpenApp} className="open-app-button">
+              Abrir Aplicativo
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
